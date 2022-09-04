4494d9d238afc2248d787f3232a7f577bb9ef09c
AnikRifat <reafatul@gmail.com>
2022-09-04 21:05:15 +0600

    first commit

---
+++ inc/header.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Boxicons CSS -->
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <!-- Slick Slider CSS -->
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/slick-theme.min.css">
+    <!-- Meanmenu JS -->
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <!-- Nice Select JS -->
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <!-- Progressbar CSS -->
+    <link rel="stylesheet" href="assets/css/progressbar.min.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Swiper Slider CSS -->
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <title>Taro | Multi-Purpose Website Templates</title>
+
+    <link rel="icon" type="image/png" href="assets/img/ride/favicon.png">
+</head>
+
+<body data-spy="scroll" data-offset="70">
+
+
+    <!-- Start Heder Area -->
+    <header class="header-area fixed-top">
+        <!-- Start Navbar Area -->
+        <div class="nav-area host-nav-area">
+            <div class="navbar-area">
+                <!-- Menu For Mobile Device -->
+                <div class="mobile-nav">
+                    <a href="app.html" class="logo">
+                        <img src="assets/img/logo.png" alt="Logo">
+                    </a>
+                </div>
+
+                <!-- Menu For Desktop Device -->
+                <div class="main-nav">
+                    <nav class="navbar navbar-expand-md">
+                        <div class="container">
+                            <a class="navbar-brand" href="app.html">
+                                <img src="assets/img/ride/logo.png" alt="Logo">
+                            </a>
+
+                            <div class="collapse navbar-collapse mean-menu">
+                                <ul class="navbar-nav m-auto">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link active">
+                                            Home
+                                            <i class="bx bx-chevron-down"></i>
+                                        </a>
+
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="app.html" class="nav-link">App (Home One)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="charity.html" class="nav-link">Charity (Home Two)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="event.html" class="nav-link">Event (Home Three)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="export.html" class="nav-link">Export (Home Four)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="host.html" class="nav-link">Hosting (Home Five)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="ride.html" class="nav-link active">Ride (Home Six)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="seo.html" class="nav-link">SEO (Home Seven)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="software.html" class="nav-link">Software (Home Eight)</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="about.html" class="nav-link">About</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">
+                                            Pages
+                                            <i class="bx bx-chevron-down"></i>
+                                        </a>
+
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="pricing.html" class="nav-link">Pricing</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="testimonials.html" class="nav-link">Testimonials</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="team.html" class="nav-link">Team</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="faq.html" class="nav-link">FAQ</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="terms-conditions.html" class="nav-link">Terms & Conditions</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="#" class="nav-link">
+                                                    User
+                                                    <i class="bx bx-chevron-right"></i>
+                                                </a>
+
+                                                <ul class="dropdown-menu">
+                                                    <li class="nav-item">
+                                                        <a href="log-in.html" class="nav-link">Log In</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a href="sign-up.html" class="nav-link">Sign Up</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a href="recover-password.html" class="nav-link">Recover Password</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="coming-soon.html" class="nav-link">Coming Soon</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="404.html" class="nav-link">404 Error Page</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">
+                                            Services
+                                            <i class="bx bx-chevron-down"></i>
+                                        </a>
+
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="services.html" class="nav-link">Services</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="services-details.html" class="nav-link">Services Details</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">
+                                            Shop
+                                            <i class="bx bx-chevron-down"></i>
+                                        </a>
+
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="shop.html" class="nav-link">Shop</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="cart.html" class="nav-link">Cart</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="checkout.html" class="nav-link">Checkout</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="product-details.html" class="nav-link">Product Details</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link">
+                                            Blog
+                                            <i class="bx bx-chevron-down"></i>
+                                        </a>
+
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="blog-grid.html" class="nav-link">Blog Grid</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="blog-details.html" class="nav-link">Blog Details</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="contact.html" class="nav-link">Contact</a>
+                                    </li>
+                                </ul>
+
+                                <!-- Start Other Option -->
+                                <div class="others-option app">
+                                    <div class="host-nav-wrap">
+                                        <div class="language">
+                                            <select>
+                                                <option>English</option>
+                                                <option>العربيّة</option>
+                                                <option>Deutsch</option>
+                                                <option>Português</option>
+                                            </select>
+                                        </div>
+                                        <div class="purchase-btn">
+                                            <a href="#">Sign up / Login</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Other Option -->
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- End Navbar Area -->
+    </header>
+    <!-- End Heder Area -->
